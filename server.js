f285decdb30a861fa2c2f2a91f2e5bc029706684
rmplcu <72087404+rmplcu@users.noreply.github.com>
2023-07-24 14:32:48 +0200

    Remove 3dview WIP

--- server.js
+++ server.js
@@ -1,34 +1,41 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const ejs = require('ejs')
-const path = require('path')
-const favicon = require('serve-favicon')
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+const favicon = require("serve-favicon");
 
-const app = express()
+const app = express();
 
-app.use(express.static(__dirname + '/public'))
-app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
-app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
-app.use(favicon(__dirname + '/public/img/favicon.ico'))
+app.use(express.static(__dirname + "/public"));
+app.use(
+  "/build/",
+  express.static(path.join(__dirname, "node_modules/three/build"))
+);
+app.use(
+  "/jsm/",
+  express.static(path.join(__dirname, "node_modules/three/examples/jsm"))
+);
+app.use(favicon(__dirname + "/public/img/favicon.ico"));
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.get('/contact', (req, res) => {
-    res.render('contact.ejs')
-})
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
 
+/*
 app.get('/3dview', (req, res) => {
     res.render('3d.ejs')
-})
+})*/
 
-app.get('/description', (req, res) => {
-    res.render('description.ejs')
-})
+app.get("/description", (req, res) => {
+  res.render("description.ejs");
+});
 
-app.listen(process.env.PORT)
-console.log(process.env.PORT)
+app.listen(process.env.PORT);
+console.log(process.env.PORT);
